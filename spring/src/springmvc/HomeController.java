e9dd2e48af3e31d4aa193e3a1c86380565d71169
vandanavanshitha <128056700+vandanavanshitha@users.noreply.github.com>
2024-05-20 15:49:45 +0530

    Add files via upload

---
+++ spring/src/springmvc/HomeController.java
@@ -0,0 +1,40 @@
+package springmvc;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import model.User;
+
+@Controller
+public class HomeController {
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/Home", method = RequestMethod.GET)
+	public String Home(Locale locale, Model model) {
+		System.out.println("Home Page Requested, locale = " + locale);
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+		String formattedDate = dateFormat.format(date);
+
+		model.addAttribute("serverTime", formattedDate);
+
+		return "Home";
+	}
+
+	@RequestMapping(value = "/user", method = RequestMethod.POST)
+	public String user(@Validated User user, Model model) {
+		System.out.println("User Page Requested");
+		model.addAttribute("userName", user.getUserName());
+		return "user";
+	}
+}
